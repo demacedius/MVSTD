5384852785e6a8c48ca04f81270d88959f5ab9e5
demacedius <demacedius@gmail.com>
2022-03-06 11:22:44 +0100

    optimisation

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 5;
 
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showgamespeed');
-showGameSpeed.innerHTML = gameSpeed;
-slider.addEventListener('change',function(e){
-    gameSpeed = e.target.value;
-    showGameSpeed.innerHTML = e.target.value;
-});
 
 const backgroundL1 = new Image();
 backgroundL1.src = 'layer-1.png';
@@ -24,51 +16,64 @@ backgroundL4.src = 'layer-4.png';
 const backgroundL5 = new Image();
 backgroundL5.src = 'layer-5.png';
 
-class Layer{
-    constructor(image, speedModifier){
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.x2 = this.width;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
-    }
-    update(){
-        this.speed = gameSpeed * this.speedModifier;
-        if (this.x <= -this.width){
-            this.x = this.width + this.x2 - this.speed;
-        }
-        if (this.x2 <= -this.width){
-            this.x2 = this.width + this.x - this.speed;
-        }
-        this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
-    }
-    draw(){
-        ctx.drawImage(this.image, this.x, this.y,this.width,this.height);
-        ctx.drawImage(this.image, this.x2, this.y,this.width,this.height);
+window.addEventListener('load', function(){
     
-    }
-}
-
-
-const layer1 = new Layer(backgroundL1, 0.2);
-const layer2 = new Layer(backgroundL2, 0.4);
-const layer3 = new Layer(backgroundL3, 0.6);
-const layer4 = new Layer(backgroundL4, 0.8);
-const layer5 = new Layer(backgroundL5, 1);
-
-const gameObject = [layer1,layer2,layer3,layer4,layer5];
-
-function animate(){
-    ctx.clearRect(0,0, CANVAS_WIDTH,CANVAS_HEIGHT);
-    gameObject.forEach(object => {
-        object.update();
-        object.draw();
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById('showgamespeed');
+    showGameSpeed.innerHTML = gameSpeed;
+    slider.addEventListener('change',function(e){
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = e.target.value;
     });
-
-    requestAnimationFrame(animate);
-};
-animate();
+    
+    class Layer{
+        constructor(image, speedModifier){
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            //this.x2 = this.width;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
+        }
+        update(){
+            this.speed = gameSpeed * this.speedModifier;
+            if (this.x <= -this.width){
+                this.x = 0;
+            }
+            /*
+            if (this.x2 <= -this.width){
+                this.x2 = this.width + this.x - this.speed;
+            }*/
+            this.x = Math.floor(this.x - this.speed);
+            //this.x2 = Math.floor(this.x2 - this.speed);
+        }
+        draw(){
+            ctx.drawImage(this.image, this.x, this.y,this.width,this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y,this.width,this.height);
+        
+        }
+    }
+    
+    
+    const layer1 = new Layer(backgroundL1, 0.2);
+    const layer2 = new Layer(backgroundL2, 0.4);
+    const layer3 = new Layer(backgroundL3, 0.6);
+    const layer4 = new Layer(backgroundL4, 0.8);
+    const layer5 = new Layer(backgroundL5, 1);
+    
+    const gameObject = [layer1,layer2,layer3,layer4,layer5];
+    
+    function animate(){
+        ctx.clearRect(0,0, CANVAS_WIDTH,CANVAS_HEIGHT);
+        gameObject.forEach(object => {
+            object.update();
+            object.draw();
+        });
+    
+        requestAnimationFrame(animate);
+    };
+    animate();
+});
